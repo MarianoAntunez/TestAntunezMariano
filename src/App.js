66618c7fcd5dbcbe60cbee9dfbcd1cf20c7247f4
Mariano Antunez <129898019+MarianoAntunez@users.noreply.github.com>
2024-07-07 01:08:48 -0300

    v1.1.1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import releaseData from "./JSON/releaseNotes.json";
 import "./App.css";
-import Image from "./utilities/images/image_10.png";
 
 const App = () => {
   useEffect(() => {
     const fadeInElements = document.querySelectorAll(".fade-in");
 
-    const handleScroll = () => {
+    const mouseScroll = () => {
       fadeInElements.forEach((element) => {
         const rect = element.getBoundingClientRect();
         if (rect.top < window.innerHeight) {
@@ -16,22 +15,24 @@ const App = () => {
       });
     };
 
-    window.addEventListener("scroll", handleScroll);
-    handleScroll();
+    window.addEventListener("scroll", mouseScroll);
+    mouseScroll();
 
     return () => {
-      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("scroll", mouseScroll);
     };
   }, []);
 
   return (
     <div>
+      <div className="progress-bar"></div>
+
       <div className="releaseNotes-Nav">
         <h1>Release Notes</h1>
       </div>
       <div className="release-notes-container">
         <div className="release-notes">
-          <section className="release fade-in">
+          <section className="release">
             <div className="sectionFilter">
               <h2>Release 6.5</h2>
               <div className="drop">
@@ -45,7 +46,8 @@ const App = () => {
             </div>
             <hr />
 
-            <div className="section section1">
+            <div className="section section1 fade-in">
+              <i className="fa-solid fa-circle indicator"></i>
               <h3>Redesigned notifications</h3>
               <div className="filters">
                 <p className="Feat">Feat</p>
@@ -62,14 +64,14 @@ const App = () => {
                 <div className="dropdowns">
                   <div className="options">
                     <div className="iconOption">
-                      <i class="fa-solid fa-inbox"></i>
+                      <i className="fa-solid fa-inbox"></i>
                       <div className="lab">
                         <label htmlFor="inbox">Inbox</label>
                         <small>Default ● CklickUp's recommended setting</small>
                       </div>
                     </div>
                     <div className="lab2">
-                      <i class="fa-solid fa-gear"></i>
+                      <i className="fa-solid fa-gear"></i>
                       <select id="inbox">
                         <option value="default">Default</option>
                         <option value="custom">Custom</option>
@@ -78,14 +80,14 @@ const App = () => {
                   </div>
                   <div className="options">
                     <div className="iconOption">
-                      <i class="fa-regular fa-envelope"></i>
+                      <i className="fa-regular fa-envelope"></i>
                       <div className="lab">
                         <label htmlFor="inbox">Email</label>
                         <small>Default ● CklickUp's recommended setting</small>
                       </div>
                     </div>
                     <div className="lab2">
-                      <i class="fa-solid fa-gear"></i>
+                      <i className="fa-solid fa-gear"></i>
                       <select id="inbox">
                         <option value="default">Default</option>
                         <option value="custom">Custom</option>
@@ -94,14 +96,14 @@ const App = () => {
                   </div>
                   <div className="options">
                     <div className="iconOption">
-                      <i class="fa-solid fa-tv"></i>
+                      <i className="fa-solid fa-tv"></i>
                       <div className="lab">
                         <label htmlFor="inbox">Browser</label>
                         <small>Default ● CklickUp's recommended setting</small>
                       </div>
                     </div>
                     <div className="lab2">
-                      <i class="fa-solid fa-gear"></i>
+                      <i className="fa-solid fa-gear"></i>
                       <select id="inbox">
                         <option value="default">Default</option>
                         <option value="custom">Custom</option>
@@ -110,14 +112,14 @@ const App = () => {
                   </div>
                   <div className="options">
                     <div className="iconOption">
-                      <i class="fa-solid fa-mobile-screen"></i>
+                      <i className="fa-solid fa-mobile-screen"></i>
                       <div className="lab">
                         <label htmlFor="inbox">Mobile</label>
                         <small>Default ● CklickUp's recommended setting</small>
                       </div>
                     </div>
                     <div className="lab2">
-                      <i class="fa-solid fa-gear"></i>
+                      <i className="fa-solid fa-gear"></i>
                       <select id="inbox">
                         <option value="default">Default</option>
                         <option value="custom">Custom</option>
@@ -128,7 +130,8 @@ const App = () => {
               </div>
             </div>
 
-            <div className="section section2">
+            <div className="section section2 fade-in">
+              <i className="fa-solid fa-circle indicator"></i>
               <h3>Bugs</h3>
               <div className="filters">
                 <p className="Fix">Fix</p>
@@ -149,7 +152,8 @@ const App = () => {
               </ul>
             </div>
 
-            <div className="section section3">
+            <div className="section section3 fade-in">
+              <i className="fa-solid fa-circle indicator"></i>
               <h3>Redesigned Time Tracking</h3>
               <div className="filters">
                 <p className="Feat">Feat</p>
@@ -161,7 +165,48 @@ const App = () => {
                     .description
                 }
               </p>
-              <img src={Image} alt="" />
+              <div className="edit-form">
+                <h2>Edit Sprint Folder</h2>
+                <div>
+                  <div className="form-group">
+                    <label>Sprint name</label>
+                    <div className="input-container">
+                      Weekly sprint (6/17 - 6/21)
+                    </div>
+                    <small>
+                      Must include {`{INDEX}`} - a sequential number for each
+                      Sprint starting with 1
+                    </small>
+                  </div>
+                  <div className="form-group">
+                    <label>Default template</label>
+                    <div className="input-container">
+                      Weekly template <i className="fa-solid fa-x"></i>
+                    </div>
+                  </div>
+                  <div className="S-P">
+                    <div className="form-group">
+                      <label>Sprint duration</label>
+                      <div className="input-container divi">
+                        2 weeks <i className="fa-solid fa-chevron-down"></i>
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <label>Date format</label>
+                      <div className="input-container divi">
+                        MM/DD <i className="fa-solid fa-chevron-down"></i>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <label>Sprint timezone</label>
+                    <div className="input-container">
+                      (GMT +01:00) Africa/Lagos (WAT)
+                      <i className="fa-solid fa-chevron-down"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </section>
         </div>
